106250a5a270ee9390a1644c660525d84c3ef5a1
Kirin Pörsti <kirin.porsti@gmail.com>
2026-09-22 09:26:47 +0000

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pubg-rangefinder-local/',
   plugins: [vue()],
   resolve: {
     alias: {
